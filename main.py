317885292296f0f9a170a1885b9c7e6f17c10b8b
Xyzbolt <Xyzzsupreme@gmail.com>
2025-09-05 17:37:10 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+
+class Timetable():
+    def __init__(self, days, lectures, sections):
+        self.dayno = days
+        self.lectureno = lectures
+        self.sections = sections
+        
+    def :
